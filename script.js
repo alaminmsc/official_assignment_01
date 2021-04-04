2c889cc15f6388e63e57175efcb3cd779518d7d2
alaminiium2019 <alamin.iium2017@gmail.com>
2021-04-04 13:01:23 +0600

    design done

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.service').mouseenter(function(){
+        $('.serviceIcon').animate({'margin-left':'60px'});         
+    }) 
+    $('.service').mouseleave(function(){
+        $('.serviceIcon').animate({'margin-left':'0px'});         
+    }) 
+
+
+})
+
+
